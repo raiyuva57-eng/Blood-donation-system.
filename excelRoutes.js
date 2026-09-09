22c12a0146535e0b1f65bfbda23fd846759dafc9
raiyuva57-eng <raiyuva57@gmail.com>
2026-09-09 21:31:02 +0530

    Update excelRoutes.js

--- excelRoutes.js
+++ excelRoutes.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const router = express.Router();
-const { importExcel, exportExcel } = require('../controllers/excelController');
-const { protect } = require('../middleware/authMiddleware');
-const { authorize } = require('../middleware/roleMiddleware');
-const upload = require('../config/multer');
+const { importExcel, exportExcel } = require('./excelController');
+const { protect } = require('./authMiddleware');
+const { authorize } = require('./roleMiddleware');
+const upload = require('./multer');
 
 router.post('/import-excel', protect, authorize('admin'), upload.single('excelFile'), importExcel);
 router.get('/export-excel', protect, authorize('admin'), exportExcel);
