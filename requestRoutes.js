f78951ba019ed59c5ba8a5450a43aa7ba7b02933
raiyuva57-eng <raiyuva57@gmail.com>
2026-09-09 21:32:37 +0530

    Update requestRoutes.js

--- requestRoutes.js
+++ requestRoutes.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const router = express.Router();
-const { getRequests, getRequestById, createRequest, updateRequest, deleteRequest } = require('../controllers/requestController');
-const { protect } = require('../middleware/authMiddleware');
-const { authorize } = require('../middleware/roleMiddleware');
+const { getRequests, getRequestById, createRequest, updateRequest, deleteRequest } = require('./requestController');
+const { protect } = require('./authMiddleware');
+const { authorize } = require('./roleMiddleware');
 
 router.get('/', protect, getRequests);
 router.get('/:id', protect, getRequestById);
