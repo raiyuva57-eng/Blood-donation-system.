a2014129568649cf8be934d459751e89a7c52ebc
raiyuva57-eng <raiyuva57@gmail.com>
2026-09-09 21:29:15 +0530

    Update donorRoutes.js

--- donorRoutes.js
+++ donorRoutes.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const router = express.Router();
-const { getDonors, getDonorById, createDonor, updateDonor, deleteDonor } = require('../controllers/donorController');
-const { protect } = require('../middleware/authMiddleware');
-const { authorize } = require('../middleware/roleMiddleware');
+const { getDonors, getDonorById, createDonor, updateDonor, deleteDonor } = require('./donorController');
+const { protect } = require('./authMiddleware');
+const { authorize } = require('./roleMiddleware');
 
 router.get('/', protect, authorize('admin', 'hospital'), getDonors);
 router.get('/:id', protect, getDonorById);
