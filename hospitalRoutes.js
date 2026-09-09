a9da8332789256c4d5b77cf72b2f27806c497135
raiyuva57-eng <raiyuva57@gmail.com>
2026-09-09 21:29:48 +0530

    Update hospitalRoutes.js

--- hospitalRoutes.js
+++ hospitalRoutes.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const router = express.Router();
-const { getHospitals, getHospitalById, createHospital, updateHospital, deleteHospital } = require('../controllers/hospitalController');
-const { protect } = require('../middleware/authMiddleware');
-const { authorize } = require('../middleware/roleMiddleware');
+const { getHospitals, getHospitalById, createHospital, updateHospital, deleteHospital } = require('./hospitalController');
+const { protect } = require('./authMiddleware');
+const { authorize } = require('./roleMiddleware');
 
 router.get('/', protect, authorize('admin'), getHospitals);
 router.get('/:id', protect, getHospitalById);
